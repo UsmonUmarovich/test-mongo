bc9d13311f7bb28fa7dbb1dc5a4096c97b03ac3b
Usmon <98887676+UsmonUmarovich@users.noreply.github.com>
2024-03-19 00:20:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routes/comment.routes.js";
 
-mongoose.connect("mongodb+srv://Usmon:<password>@usmon.dqfejre.mongodb.net/?retryWrites=true&w=majority&appName=Usmon");
+mongoose.connect("mongodb+srv://Usmon:17032009aA@usmon.dqfejre.mongodb.net/?retryWrites=true&w=majority&appName=Usmon");
 const db = mongoose.connection;
 
 db.on("error", (err) => console.error);
