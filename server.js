449117cb01a923de084ce5a9a17dbee94830046f
Usmon Umarovich <usmon@Usmons-MacBook-Pro.local>
2024-03-19 00:24:21 +0500

    updates

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import router from "./routes/comment.routes.js";
+import Comments from "./models/comments.js";
 
-mongoose.connect(DB);
+mongoose.connect(
+  "mongodb+srv://Usmon:17032009aA@usmon.dqfejre.mongodb.net/?retryWrites=true&w=majority&appName=Usmon"
+);
 const db = mongoose.connection;
 
 db.on("error", (err) => console.error);
 
-db.once("open" , () => console.log("Database connection open"))
+db.once("open", () => console.log("Database connection open"));
 
 const app = express();
 
@@ -16,14 +18,27 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
-const DB = process.env.MONGODB_URL
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/comments", async (req, res) => {
+  await Comments.find()
+    .then((r) => res.json(r))
+    .catch((err) => res.status(500).json({ message: "Error: " + err }));
 });
 
-app.use(router)
+app.post("/comments", async (req, res) => {
+  const comment = new Comments({
+    comment: req.body.comment,
+  });
+  await comment
+    .save()
+    .then(() =>
+      res.status(200).json({ message: "Comment posted successfully" })
+    )
+    .catch((err) => res.status(500).json({ message: "Error: " + err }));
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server running on port 3000`);
 });
