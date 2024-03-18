6d24cb9fe2fccd51f053e706bbaffdb161a9d1a6
Usmon Umarovich <usmon@Usmons-MacBook-Pro.local>
2024-03-18 23:09:50 +0500

    changes

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,16 +1,16 @@
 import Comments from "../models/comments.js";
 
-export const getAllComments = (req, res) => {
-  Comments.find()
+export const getAllComments = async (req, res) => {
+  await Comments.find()
     .then((r) => res.json(r))
     .catch((err) => res.status(500).json({ message: "Error: " + err }));
 };
 
-export const postComment = (req, res) => {
+export const postComment = async (req, res) => {
   const comment = new Comments({
     comment: req.body.comment,
   });
-  comment
+  await comment
     .save()
     .then(() =>
       res.status(200).json({ message: "Comment posted successfully" })
