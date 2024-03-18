dcf79657969a0cde9d761571fdc4a16894da34bc
Usmon Umarovich <usmon@Usmons-MacBook-Pro.local>
2024-03-18 20:00:32 +0500

    1

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+mongoose.connect("mongodb://localhost:27017/");
+const db = mongoose.connection;
+
+db.on("error", (err) => console.error);
+
+db.once("open" , () => console.log("Database connection open"))
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
